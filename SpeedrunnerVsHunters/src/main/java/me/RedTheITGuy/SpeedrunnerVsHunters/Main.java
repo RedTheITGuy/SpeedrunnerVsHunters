913d7f029fa3b815230cda235efd0da256ac9aa7
red <red@localhost.localdomain>
2020-08-13 20:08:13 +0100

    Added world creation and deletion

--- SpeedrunnerVsHunters/src/main/java/me/RedTheITGuy/SpeedrunnerVsHunters/Main.java
+++ SpeedrunnerVsHunters/src/main/java/me/RedTheITGuy/SpeedrunnerVsHunters/Main.java
@@ -1,4 +1,5 @@
 package me.RedTheITGuy.SpeedrunnerVsHunters;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -6,10 +7,13 @@ public class Main extends JavaPlugin {
     public void onEnable() {
 	// Creates the config if it doesn't exist 
 	this.saveDefaultConfig();
-    }
-    
-    // Runs when the plugin is disabled
-    public void onDisable() {
 	
+	// Loads the config
+	FileConfiguration config = this.getConfig();
+	// Gets info from the config
+	boolean loadPrevious = config.getBoolean("persistantSave");
+	
+	// Generates the worlds needed (and deletes old versions if necessary)
+	GenerateWorlds.generate(!loadPrevious);
     }
 }
