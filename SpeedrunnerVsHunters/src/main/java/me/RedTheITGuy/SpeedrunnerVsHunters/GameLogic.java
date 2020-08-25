e07e832ed7214df79c470f9b8ea1312c4cadef83
red <red@localhost.localdomain>
2020-08-25 20:45:00 +0100

    Added timer countdown

--- SpeedrunnerVsHunters/src/main/java/me/RedTheITGuy/SpeedrunnerVsHunters/GameLogic.java
+++ SpeedrunnerVsHunters/src/main/java/me/RedTheITGuy/SpeedrunnerVsHunters/GameLogic.java
@@ -1,4 +1,8 @@
 package me.RedTheITGuy.SpeedrunnerVsHunters;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.NamespacedKey;
@@ -19,13 +23,16 @@ public class GameLogic {
 		// Loads the config
 		FileConfiguration config = Bukkit.getPluginManager().getPlugin("SpeedrunnerVsHunters").getConfig();
 		// Gets info from the config
-		int headStart = config.getInt("game.headStart");
-		int locationRevealTime = config.getInt("game.locationRevealTime");
+		final int headStart = config.getInt("game.headStart");
+		final int locationRevealTime = config.getInt("game.locationRevealTime");
+		
+		// Teleports the runner into the world
+		runner.teleport(Bukkit.getWorld("svh-overworld").getSpawnLocation());
 		
 		// Gets the scoreboard manager
 		ScoreboardManager scoreboardManager = Bukkit.getScoreboardManager();
 		// Gets the scoreboard
-		Scoreboard scoreboard = scoreboardManager.getMainScoreboard();
+		final Scoreboard scoreboard = scoreboardManager.getMainScoreboard();
 		
 		// Gets the speedrunner team
 		Team speedrunnerTeam = scoreboard.getTeam("speedrunner");
@@ -91,7 +98,7 @@ public class GameLogic {
 		infoBoard.getScore(ChatColor.AQUA + "Time: ").setScore(0);
 		
 		// Creates the key for the boss bar
-		NamespacedKey barKey = new NamespacedKey(Bukkit.getPluginManager().getPlugin("SpeedrunnerVsHunters"), "svhBar");
+		final NamespacedKey barKey = new NamespacedKey(Bukkit.getPluginManager().getPlugin("SpeedrunnerVsHunters"), "svhBar");
 		// Gets the boss bar
 		KeyedBossBar bossBar = Bukkit.getServer().getBossBar(barKey);
 		// Runs if the bar doesn't exist
@@ -111,9 +118,6 @@ public class GameLogic {
 		bossBar.setProgress(1.0);
 		// Sets the title of the boss bar
 		bossBar.setTitle(ChatColor.GOLD + "Hunters released in " + headStart + ":00");
-		
-		// Teleports the runner into the world
-		runner.teleport(Bukkit.getWorld("svh-overworld").getSpawnLocation());
 
 		// Runs for every player
 		for (Player player : Bukkit.getServer().getOnlinePlayers()) {
@@ -127,5 +131,117 @@ public class GameLogic {
 			// Adds the player to the hunters team if they are not the runner
 			if (!player.equals(runner)) huntersTeam.addEntry(player.getName());
 		}
+		
+		// Creates a new schedular for the next code
+		ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
+		// Runs every second
+		ses.scheduleAtFixedRate(new Runnable() {
+			public void run() {
+				// Gets the objective
+				Objective infoBoard = scoreboard.getObjective("svhGameInfo");
+				// Runs if the game is still running
+				if (!infoBoard.getScore(ChatColor.AQUA + "Winner: ").isScoreSet()) {
+					// Gets the current run time with minutes and seconds split
+					String[] runTime = scoreboard.getTeam("runTime").getSuffix().split(":");
+					int hoursSurvived = 0;
+					int minsSurvived = 0;
+					int secsSurvived = 0;
+					if (runTime.length <= 2) {
+						// Converts the minutes and seconds to integers
+				    	minsSurvived = Integer.parseInt(runTime[0]);
+				    	secsSurvived = Integer.parseInt(runTime[1]);
+					}
+					else {
+						// Converts the hours, minutes and seconds to integers
+						hoursSurvived = Integer.parseInt(runTime[0]);
+				    	minsSurvived = Integer.parseInt(runTime[1]);
+				    	secsSurvived = Integer.parseInt(runTime[2]);
+					}
+			    	
+			    	// Adds 1 to the seconds
+			    	secsSurvived++;
+			    	// Runs if seconds if 60+
+			    	if (secsSurvived > 59) {
+			    		// Resets the seconds to 0
+			    		secsSurvived = 0;
+			    		// Adds 1 to the minutes
+			    		minsSurvived++;
+			    		// Runs if minutes is 60+
+			    		if (minsSurvived > 59) {
+			    			// Resets the minutes to 0
+			    			minsSurvived = 0;
+			    			// Adds 1 to the hours
+			    			hoursSurvived++;
+			    		}
+			    	}
+			    	
+			    	// Creates the string to store the time suffix
+			    	String runTimeString = "";
+			    	// Runs if there are hours in the run time
+			    	if (hoursSurvived > 0) {
+			    		runTimeString = hoursSurvived + ":" + String.format("%02d", minsSurvived) + ":" + String.format("%02d", secsSurvived);
+			    	}
+			    	else {
+			    		runTimeString = minsSurvived + ":" + String.format("%02d", secsSurvived);
+			    	}
+			    	
+			    	// Sets the new time in the scoreboard
+			    	scoreboard.getTeam("runTime").setSuffix(runTimeString);
+				}
+				
+				// Gets the boss bar
+				KeyedBossBar bossBar = Bukkit.getServer().getBossBar(barKey);
+				// Runs if there is a boss bar
+				if (bossBar != null) {
+					// Gets the boss bar title
+					String barTitle = bossBar.getTitle();
+					// Strips the colour so it doesn't mess up the code
+					barTitle = ChatColor.stripColor(barTitle);
+					// Gets the time from the boss bar
+					String[] barTime = barTitle.replaceAll("[^:\\d]", "").split(":");
+					// Converts the minutes and seconds to integers
+			    	int minsLeft = Integer.parseInt(barTime[0]);
+			    	int secsLeft = Integer.parseInt(barTime[1]);
+			    	
+			    	// Subtracts 1 from the seconds
+			    	secsLeft--;
+			    	// Runs if the seconds left has reached 0
+			    	if (secsLeft < 1) {
+			    		// Resets the seconds to 59
+			    		secsLeft = 59;
+			    		// Subtracts 1 from the minutes
+			    		minsLeft--;
+			    		// Runs if the minutes left has reached 0
+			    		if (minsLeft < 0) {
+			    			// Sets the seconds to 0
+			    			secsLeft = 0;
+			    			// Sets the minutes to the location reveal time
+			    			minsLeft = locationRevealTime;
+			    			// Sets the bar title to show it's now for location reveal
+			    			barTitle = "Location revealed in ";
+			    		}
+			    	}
+			    	// Changes the title string to what the title will be
+			    	barTitle = barTitle.replaceAll("[:\\d]", "") + minsLeft + ":" + String.format("%02d", secsLeft);
+			    	// Sets the bars title
+			    	bossBar.setTitle(ChatColor.GOLD + barTitle);
+			    	
+			    	// Creates a double to store the bar progress
+			    	double barProgress = 0;
+			    	// Runs if the timer is for the initial release
+			    	if (barTitle.contains("Hunters released in")) {
+			    		// Calculates the bar progress
+			    		barProgress = (((double) minsLeft * 60) + secsLeft) / (headStart * 60);
+			    	}
+			    	// Runs if the timer is for the location reveal
+			    	else if (barTitle.contains("Location revealed in")) {
+			    		// Calculates the bar progress
+			    		barProgress = (((double) minsLeft * 60) + secsLeft) / (locationRevealTime * 60);
+			    	}
+			    	// Sets the bars progress
+			    	bossBar.setProgress(barProgress);
+				}
+			}
+		}, 1, 1, TimeUnit.SECONDS);
 	}
 }
